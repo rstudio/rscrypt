f83d2c54f52d2c5303c5f343aea27b5d2e896c02
Andy Kipp <andy@rstudio.com>
2014-01-08 13:57:16 -0500

    Fix tabs

--- src/base64.cpp
+++ src/base64.cpp
@@ -16,12 +16,11 @@ static const char cb64[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01
  *
  * encode 3 8-bit binary bytes as 4 '6-bit' characters
  */
-void encodeblock( unsigned char in[3], unsigned char out[4], int len )
-{
-  out[0] = cb64[ in[0] >> 2 ];
-  out[1] = cb64[ ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4) ];
-  out[2] = (unsigned char) (len > 1 ? cb64[ ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) ] : '=');
-  out[3] = (unsigned char) (len > 2 ? cb64[ in[2] & 0x3f ] : '=');
+void encodeblock( unsigned char in[3], unsigned char out[4], int len ) {
+    out[0] = cb64[ in[0] >> 2 ];
+    out[1] = cb64[ ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4) ];
+    out[2] = (unsigned char) (len > 1 ? cb64[ ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) ] : '=');
+    out[3] = (unsigned char) (len > 2 ? cb64[ in[2] & 0x3f ] : '=');
 }
 
 /*
@@ -29,13 +28,12 @@ void encodeblock( unsigned char in[3], unsigned char out[4], int len )
  *
  * decode 4 '6-bit' characters to 3 8-bit bytes
  */
-void decodeblock(unsigned char in[4], unsigned char out[3], int len) 
-{
-  unsigned char ch[len];
-  for (int i = 0; i < 4; i++)
-      ch[i] = strchr(cb64, in[i]) - cb64;
+void decodeblock(unsigned char in[4], unsigned char out[3], int len) {
+    unsigned char ch[len];
+    for (int i = 0; i < 4; i++)
+        ch[i] = strchr(cb64, in[i]) - cb64;
 
-  out[0] = (ch[0] << 2) + ((ch[1] & 0x30) >> 4);
-  out[1] = (len > 2 ? ((ch[1] & 0xf) << 4) + ((ch[2] & 0x3c) >> 2) : 0);
-  out[2] = (len > 3 ? ((ch[2] & 0x3) << 6) + ch[3] : 0);
+    out[0] = (ch[0] << 2) + ((ch[1] & 0x30) >> 4);
+    out[1] = (len > 2 ? ((ch[1] & 0xf) << 4) + ((ch[2] & 0x3c) >> 2) : 0);
+    out[2] = (len > 3 ? ((ch[2] & 0x3) << 6) + ch[3] : 0);
 }
--- src/base64.hpp
+++ src/base64.hpp
@@ -7,65 +7,63 @@ void decodeblock( unsigned char in[4], unsigned char out[3], int len );
 template <class InputIterator>
 std::string b64encode(InputIterator begin, InputIterator end)
 {
-   unsigned char in[3], out[4];
-   int i, len;
+    unsigned char in[3], out[4];
+    int i, len;
 
-   std::string databuf;
+    std::string databuf;
 
-   while( begin != end ) {
-      len = 0;
-      for( i = 0; i < 3; i++ ) {
-         if( begin != end ) {
-            in[i] = static_cast<unsigned char>( *begin++ );
-            len++;
-         } else {
-            in[i] = 0;
-         }
-      }
-      if( len ) {
-         encodeblock( in, out, len );
-         for( i = 0; i < 4; i++ ) {
-            databuf.push_back(out[i]);
-         }
-      }
-   }
-
-   return databuf;
+    while( begin != end ) {
+        len = 0;
+        for( i = 0; i < 3; i++ ) {
+            if( begin != end ) {
+                in[i] = static_cast<unsigned char>( *begin++ );
+                len++;
+            } else {
+                in[i] = 0;
+            }
+        }
+        if( len ) {
+            encodeblock( in, out, len );
+            for( i = 0; i < 4; i++ ) {
+                databuf.push_back(out[i]);
+            }
+        }
+    }
+    return databuf;
 }
 
 template <class InputIterator>
 std::string b64decode(InputIterator begin, InputIterator end)
 {
-   unsigned char in[4], out[3];
-   unsigned char tmp;
-   int i, len;
+    unsigned char in[4], out[3];
+    unsigned char tmp;
+    int i, len;
 
-   std::string databuf;
+    std::string databuf;
 
-   while( begin != end ) {
-      len = 0;
-      for( i = 0; i < 4; i++ ) {
-         if( begin != end ) {
-            tmp = static_cast<unsigned char>( *begin++ );
-            if (tmp != '=') {
-                in[i] = tmp;
-                len++;
+    while( begin != end ) {
+        len = 0;
+        for( i = 0; i < 4; i++ ) {
+            if( begin != end ) {
+                tmp = static_cast<unsigned char>( *begin++ );
+                if (tmp != '=') {
+                    in[i] = tmp;
+                    len++;
+                } else {
+                    in[i] = 0;
+                }
             } else {
                 in[i] = 0;
             }
-         } else {
-            in[i] = 0;
-         }
-      }
-      if( len ) {
-         decodeblock( in, out, len );
-         for( i = 0; i < 3; i++ ) {
-            databuf.push_back(out[i]);
-         }
-      }
-   }
-
-   return databuf;
+        }
+        if( len ) {
+            decodeblock( in, out, len );
+            for( i = 0; i < 3; i++ ) {
+                databuf.push_back(out[i]);
+            }
+        }
+    }
+    return databuf;
 }
 
 #endif
