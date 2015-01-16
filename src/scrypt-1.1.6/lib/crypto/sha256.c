282d858409634abada4923210693591231dae364
Andy Kipp <andy@rstudio.com>
2015-01-16 12:33:07 -0500

    Rename PBKDF2_SHA256 to scrypt_PBKDF2_SHA256

--- src/scrypt-1.1.6/lib/crypto/sha256.c
+++ src/scrypt-1.1.6/lib/crypto/sha256.c
@@ -355,12 +355,12 @@ scrypt_HMAC_SHA256_Final(unsigned char digest[32], HMAC_SHA256_CTX * ctx)
 }
 
 /**
- * PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
+ * scrypt_PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
  * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
  * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
  */
 void
-PBKDF2_SHA256(const uint8_t * passwd, size_t passwdlen, const uint8_t * salt,
+scrypt_PBKDF2_SHA256(const uint8_t * passwd, size_t passwdlen, const uint8_t * salt,
     size_t saltlen, uint64_t c, uint8_t * buf, size_t dkLen)
 {
 	HMAC_SHA256_CTX PShctx, hctx;
--- src/scrypt-1.1.6/lib/crypto/sha256.h
+++ src/scrypt-1.1.6/lib/crypto/sha256.h
@@ -52,11 +52,11 @@ void	scrypt_HMAC_SHA256_Update(HMAC_SHA256_CTX *, const void *, size_t);
 void	scrypt_HMAC_SHA256_Final(unsigned char [32], HMAC_SHA256_CTX *);
 
 /**
- * PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
+ * scrypt_PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
  * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
  * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
  */
-void	PBKDF2_SHA256(const uint8_t *, size_t, const uint8_t *, size_t,
+void	scrypt_PBKDF2_SHA256(const uint8_t *, size_t, const uint8_t *, size_t,
     uint64_t, uint8_t *, size_t);
 
 #endif /* !_SHA256_H_ */
